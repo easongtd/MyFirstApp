6f6b67ed0a681484e3f23c8be16635dc19a959ee
easongtd <eason.gtd@gmail.com>
2016-04-06 15:13:29 +0800

    MyFirstApp
    MyFirstApp

---
+++ MyFirstApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstApp
+//
+//  Created by eason on 15/6/20.
+//  Copyright (c) 2015年 eason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyFirstApp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MyFirstApp
+//
+//  Created by eason on 15/6/20.
+//  Copyright (c) 2015年 eason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSLog(@"MyFirstApp");
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
